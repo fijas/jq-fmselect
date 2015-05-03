4aab8052c4ebaa6764752cd7fc94754c6f374daf
Fijas Pocker <fijas@hashsolutions.in>
2015-05-03 12:54:20 +0530

    Lots of fixes and introduced new option listHeight

--- fmselect.js
+++ fmselect.js
@@ -11,27 +11,22 @@
 	$.fn.fmSelect = function (options) {
 		var widgetCount = 1;
 		var settings = $.extend({
-			// Default Values
-			title: "Select Values",
-			width: "100%"
+			title: "Select Options",
+			width: "100%",
+			listHeight: "130px"
 		}, options);
 		return this.each(function () {
 			var count = 0;
 			var widgetId = "";
-			if ($(this).prop("id") !== "") {
-				widgetId = $(this).prop("id");
-			} else {
-				widgetId = "fmselect-" + widgetCount;
-			}
-			var widgetName = $(this).prop("name") + "[]";
-			if ($(this).data("title") !== undefined) {
-				settings.title = $(this).data("title");
-			}
-			var html = '<div class="fmselect" id="' + widgetId + ' style="'+settings.width+'"><div class="fmselect-head"><div class="fmselect-all-container"><a href="#" class="fmselect-all">All</a></div><div class="fmselect-head-text">' + settings.title + '</div><div class="fmselect-clear-container"><a href="#" class="fmselect-clear">Clear</a></div></div><div class="fmselect-search-container"><input type="text" class="fmselect-search" autocomplete="off" /></div><div class="fmselect-list"><ul>';
+			if ($(this).prop("id") !== "") { widgetId = $(this).prop("id"); } else { widgetId = "fmselect-" + widgetCount; }
+			var widgetName = $(this).prop("name");
+			if ($(this).data("title") !== undefined) { settings.title = $(this).data("title"); }
+			var html = '<div class="fmselect" id="' + widgetId + '" style="width: '+settings.width+';"><div class="fmselect-head"><div class="fmselect-all-container"><a href="#" class="fmselect-all">All</a></div><div class="fmselect-head-text">' + settings.title + '</div><div class="fmselect-clear-container"><a href="#" class="fmselect-clear">Clear</a></div></div><div class="fmselect-search-container"><input type="text" class="fmselect-search" autocomplete="off" /></div><div class="fmselect-list" style="height: '+settings.listHeight+';"><ul>';
+			var checked = "";
 			$(this).children("option").each(function () {
 				++count;
-				html += "<li><input type='checkbox' name='" +  widgetName + "' id='fmselect-val-" + widgetId + "-" + count + "' value='" + $(this).val() + "' data-text='" + $(this).text() + "'/> <label for='fmselect-val-" + widgetId + "-" + count + "'>" + $(this).text() + "</label></li>";
-
+				if($(this).prop("selected") === true){ checked = "checked"; }else{ checked = ""; }
+				html += "<li class='"+checked+"'><input type='checkbox' name='" +  widgetName + "' id='fmselect-val-" + widgetId + "-" + count + "' value='" + $(this).val() + "' data-text='" + $(this).text() + "' "+checked+"/> <label for='fmselect-val-" + widgetId + "-" + count + "'>" + $(this).text() + "</label></li>";
 			});
 			html += '</ul></div><div class="fmselect-selected">Selected (<span class="fmselect-count">0</span>): <span class="fmselect-selected-text"></span></div>';
 			$(this).replaceWith(html);
@@ -57,17 +52,22 @@
 		$("#" + ID).on("keyup", "input.fmselect-search", function (e) {
 			performSearch(ID, $(this).val());
 		});
+		updateSelected(ID);
 	}
 
 	function updateSelected(ID) {
-		var count = 0;
-		var selected = "";
+		var count = 0, selected = "";
 		$("#" + ID + " .fmselect-list li > input:checked").each(function () {
 			++count;
-			$(this).parent().addClass("selected");
+			$(this).parent().addClass("checked");
 			selected += $(this).data("text") + ", ";
 		});
-		$("#" + ID + " .fmselect-list li > input:not(:checked)").parent().removeClass("selected");
+		if(count > 0 && count === $("#" + ID + " .fmselect-list li > input").length){
+			selected = "<em>All</em>";
+		}else if(count === 0){
+			selected = "<em>None</em>";
+		}
+		$("#" + ID + " .fmselect-list li > input:not(:checked)").parent().removeClass("checked");
 		$("#" + ID + " .fmselect-count").html(count);
 		$("#" + ID + " .fmselect-selected-text").html(selected);
 	}
